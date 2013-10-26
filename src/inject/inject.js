11758ca6927d06955cf9e61e8e889b24206c0048
Harrison Shoff <hi@hshoff.com>
2013-10-26 13:15:01 -0700

    [inject] add maestro, keyboard in/outdent

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,13 +2,33 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
   	if (document.readyState === "complete") {
   		clearInterval(readyStateCheckInterval);
+      var $ = jQuery;
 
   		// This part of the script triggers when page is done loading
   		console.log("Howdy from Hemingway.");
 
       // Hemingway
-      document.getElementById('padbody').classList.add('hemingway');
+      var $body = $('#padbody'),
+          $toolbar = $body.find('#mainbar'),
+          $indentButton = $toolbar.find('#indentbutton'),
+          $outdentButton = $toolbar.find('#outdentbutton');
 
+      $body.addClass('hemingway');
+
+      maestro.registerAll([{
+        shortcut: 'alt+[',
+        fn: function(event) {
+          event.preventDefault();
+          $outdentButton.click();
+        }
+      },{
+        shortcut: 'alt+]',
+        fn: function(event) {
+          event.preventDefault();
+          $indentButton.click();
+        }
+      }
+    ]);
 
   	}
 	}, 10);
